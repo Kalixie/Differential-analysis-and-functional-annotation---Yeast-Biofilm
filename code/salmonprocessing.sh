97edf5be6c47f04f6db7f68924aa04d0ae62bb78
Nadira <flowerbunny1234@gmail.com>
2026-02-27 00:11:43 -0500

    Add files via upload

---
+++ code/salmonprocessing.sh
@@ -0,0 +1,43 @@
+conda install fastqc 
+
+fastqc *.fastq
+
+# Trim
+
+conda install fastp
+
+for f in *.fastq
+do
+    fastp -i $f -o trimmed_$f -h ${f%.fastq}_fastp.html -j ${f%.fastq}_fastp.json
+done
+
+# fastqc again
+
+fastqc trimmed_*.fastq
+
+# Worse results, using original data
+
+# Download reference transcriptome
+
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/146/045/GCF_000146045.2_R64/GCF_000146045.2_R64_rna.fna.gz
+
+gunzip GCF_000146045.2_R64_rna.fna.gz
+
+# Convert reads to gene counts
+
+conda install salmon
+
+salmon index -t GCF_000146045.2_R64_rna.fna -i yeastindex
+
+# Run for all 9
+
+for f in *.fastq
+do
+  salmon quant -i yeastindex -l A -r $f -p 4 -o ${f%.fastq}_quant
+done
+
+# Now have quant.sf files need for R
+
+
+
+
